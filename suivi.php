635eddf128927af6f24752f64076dee1cbca3e60
Adouaboubarnabas <99972329+Adouaboubarnabas@users.noreply.github.com>
2022-04-24 21:39:02 +0200

    Add files via upload

---
+++ suivi.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <script src="./js/bootstrap.js"></script>
+    <title>Suivi</title>
+</head>
+<body>
+  <header class="container-fluid">
+        <img src="./images/logo.svg.png" class="para1">
+        <h3 class="para2">Connexion </h3>
+  </header>
+  <section class="corpsdepagedeux">
+        <div class="barrebleu" style="background-color:#86bc25 !important;">
+          
+        </div>
+      <div class="enregistrement container"style="color:#86bc25 !important;">
+            <div class=" partieabonnementform">
+          <table class="table container-fluid" style="color:#86bc25 !important;">
+          <thead class="thead-light" style="text-align:center;">
+          <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Date de Naissance</th>
+            <th>Debut d'Abonnement</th>
+            <th>Fin d'abonnement</th>
+            <th>Email</th>
+            <th>Télephone</th>
+          </tr>
+          </thead>
+          <tbody style="text-align:center;">
+          <tr>
+            <td>Adouabou</td>
+            <td>Barnabas</td>
+            <td>07/06/1997</td>
+            <td>20/04/2022</td>
+            <td>19/04/2023</td>
+            <td>mradouabou97@gmail.com</td>
+            <td>+22607676579</td>
+          </tr>
+          <tr>
+            <td>Adouabou</td>
+            <td>Barnabas</td>
+            <td>07/06/1997</td>
+            <td>20/04/2022</td>
+            <td>19/04/2023</td>
+            <td>mradouabou97@gmail.com</td>
+            <td>+22607676579</td>
+          </tr>          <tr>
+            <td>Adouabou</td>
+            <td>Barnabas</td>
+            <td>07/06/1997</td>
+            <td>20/04/2022</td>
+            <td>19/04/2023</td>
+            <td>mradouabou97@gmail.com</td>
+            <td>+22607676579</td>
+          </tr>
+          </tbody>
+          </table>
+           </div>
+      </div>
+    </section>
+  <footer class="container-fluid pieddepasse2">
+       <div>
+           <h5>©Agence Universitaire De La francophonie 2022 Tous droits réservés</h5>
+       </div>
+  </footer>
+</body>
+</html>
